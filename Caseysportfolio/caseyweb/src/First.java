ca4410d3ece1f95248e4269302acbcdc3938522d
Casey <cbaldwin1992@gmail.com>
2019-11-06 17:30:39 -0800

    Added copy of servlets

---
+++ Caseysportfolio/caseyweb/src/First.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class First extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet() {
+//response.setContentType("text/html");
+}
+}
