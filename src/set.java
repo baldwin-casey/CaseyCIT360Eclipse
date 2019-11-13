db2e7e08d001a1f656fe2dce05f47a1bef8df2f4
Casey Baldwin <cbaldwin1992@gmail.com>
2019-11-13 04:26:16 -0800

    Added files

---
+++ src/set.java
@@ -0,0 +1,69 @@
+
+
+public class set {
+	public static void set() {
+		
+	}
+	public HashSet createset() {
+		set<String> deck = new HashSet<String>();
+	 deck.add(count, "2 of Hearts");
+    deck.add(count++, "3 of Hearts");
+    deck.add(count++, "4 of Hearts");
+    deck.add(count++, "5 of Hearts");
+    deck.add(count++, "6 of Hearts");
+    deck.add(count++, "7 of Hearts");
+    deck.add(count++, "8 of Hearts");
+    deck.add(count++, "9 of Hearts");
+    deck.add(count++, "10 of Hearts");
+    deck.add(count++, "Jack of Hearts");
+    deck.add(count++, "Queen of Hearts");
+    deck.add(count++, "King of Hearts");
+    deck.add(count++, "Ace of Hearts");
+    deck.add(count++, "2 of Diamonds");
+    deck.add(count++, "3 of Diamonds");
+    deck.add(count++, "4 of Diamonds");
+    deck.add(count++, "5 of Diamonds");
+    deck.add(count++, "6 of Diamonds");
+    deck.add(count++, "7 of Diamonds");
+    deck.add(count++, "8 of Diamonds");
+    deck.add(count++, "9 of Diamonds");
+    deck.add(count++, "10 of Diamonds");
+    deck.add(count++, "Jack of Diamonds");
+    deck.add(count++, "Queen of Diamonds");
+    deck.add(count++, "King of Diamonds");
+    deck.add(count++, "Ace of Diamonds");
+    deck.add(count++, "2 of Spades");
+    deck.add(count++, "3 of Spades");
+    deck.add(count++, "4 of Spades");
+    deck.add(count++, "5 of Spades");
+    deck.add(count++, "6 of Spades");
+    deck.add(count++, "7 of Spades");
+    deck.add(count++, "8 of Spades");
+    deck.add(count++, "9 of Spades");
+    deck.add(count++, "10 of Spades");
+    deck.add(count++, "Jack of Spades");
+    deck.add(count++, "Queen of Spades");
+    deck.add(count++, "King of Spades");
+    deck.add(count++, "Ace of Spades");
+    deck.add(count++, "2 of Clubs");
+    deck.add(count++, "3 of Clubs");
+    deck.add(count++, "4 of Clubs");
+    deck.add(count++, "5 of Clubs");
+    deck.add(count++, "6 of Clubs");
+    deck.add(count++, "7 of Clubs");
+    deck.add(count++, "8 of Clubs");
+    deck.add(count++, "9 of Clubs");
+    deck.add(count++, "10 of Clubs");
+    deck.add(count++, "Jack of Clubs");
+    deck.add(count++, "Queen of Clubs");
+    deck.add(count++, "King of Clubs");
+    deck.add(count++, "Ace of Clubs");
+	return deck;
+	}
+	
+	public deleteset(deck) {
+		for (int i = 0, deck.size() > i, i++) {
+		deck.delete(i);
+		}			
+	}
+}
